ac208b156a355c6ec8301fef2a78a5524ac79fd9
falcoleonel <40280639+falcoleonel@users.noreply.github.com>
2020-01-21 23:46:57 -0600

    Practice 5
    Use of properties in components

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Robert Smith",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJQvfY_PSYM2xiK9PXXuniqf3t3i_wLAy71e-LVqKeJC-BKz2t",
+    tel: "3333331020",
+    email: "robert@smith.com"
+  },
+  {
+    name: "David Bowie",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5vad3Y2OWI5ZPqcYlcp8jfNwDnWuS2HLYm4nQvvzihQn0rosf",
+    tel: "3333331020",
+    email: "david@bowie.com"
+  },
+  {
+    name: "RHCP",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFPAAFgFfnLsayVNKmrE4pUejXD3QLIF54ul52h54vm-cCP8ah",
+    tel: "3333331020",
+    email: "rh@cp.com"
+  }
+];
+
+export default contacts;
