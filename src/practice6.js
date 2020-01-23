ccc5ba09ab1dd2cf54c6ad7799c1dd33e6cac141
falcoleonel <40280639+falcoleonel@users.noreply.github.com>
2020-01-22 22:27:23 -0600

    Substring
    Use of function mapping an substring

--- src/practice6.js
+++ src/practice6.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import emojipedia from "./emojipedia";
 
 import "./styles6.css";
+
+const shortMeaning = emojipedia.map(function(x) {
+  return x.meaning.substring(0, 100);
+});
+
+console.log(shortMeaning);
 
 ReactDOM.render(<App />, document.getElementById("root"));
