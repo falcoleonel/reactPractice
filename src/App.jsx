68d3ac44c28d8369681e67defb5a2b1cf63a4158
falcoleonel <40280639+falcoleonel@users.noreply.github.com>
2020-01-22 19:55:58 -0600

    React DevTools
    Using of react developer tools making smaller components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./components/Card";
+import Avatar from "./components/Avatar";
 import contacts from "./contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSxJN4jtTcIGnVsvTO-sohQ0o8GfZBBwdMXSiM4YUg_RXrhbobZ" />
       <Card
         name={contacts[0].name}
         img={contacts[0].img}
